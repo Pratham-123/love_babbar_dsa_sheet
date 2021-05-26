37141da836c232ad1bfc26a0bcbbedff7490cdbe
Pratham-123 <54859695+Pratham-123@users.noreply.github.com>
2021-05-26 22:43:58 +0530

    #8

---
+++ #8.largest_sum_contigious_subarray.cpp
@@ -0,0 +1,22 @@
+  int maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        int max = INT_MIN;
+        
+        int max_till_here = 0;
+        
+        for(int i=0;i<n;i++)
+        {
+            max_till_here = max_till_here + arr[i];
+            
+            if(max_till_here>max){
+                max= max_till_here;
+            }
+            if(max_till_here<0)
+            {
+                max_till_here=0;
+            }
+        }
+        return max;
+        
+    }
