eea394e6dc41d214a9dcdc48d42ea9d1c4dde291
Pratham-123 <54859695+Pratham-123@users.noreply.github.com>
2021-05-26 23:56:19 +0530

    1

--- #8.largest_sum_contigious_subarray.cpp
+++ #8.largest_sum_contigious_subarray.cpp
@@ -1,25 +1,22 @@
-class Solution {
-  public:
-    int getMinDiff(int arr[], int n, int k) {
-        // code here
+int maxSubarraySum(int arr[], int n){
         
-        sort(arr,arr+n);
-        int ans = arr[n-1]- arr[0];
+        // Your code here
+        int max = INT_MIN;
         
-        int smallest =arr[0]+k;
-        int largest = arr[n-1]-k;
-        int mi, ma;
+        int max_till_here = 0;
         
-        for(int i=0;i<n-1;i++)
+        for(int i=0;i<n;i++)
         {
-            mi= min(smallest, arr[i+1]-k);
-            ma= max(largest,arr[i]+k);
+            max_till_here = max_till_here + arr[i];
             
-            if(mi<0) continue;
-            
-            ans = min(ans,ma-mi);
+            if(max_till_here>max){
+                max= max_till_here;
+            }
+            if(max_till_here<0)
+            {
+                max_till_here=0;
+            }
         }
-        return ans;
-         
+        return max;
+        
     }
-};
